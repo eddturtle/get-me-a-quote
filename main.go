8e6805c5c3cc3c5fc0ff11f1bb851d00fe6c137b
Edd Turtle <eddturtle@gmail.com>
2017-10-03 20:48:20 +0100

    Update api to handle json/xml && add readme

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
+//
+// Tiny API to work as a random 'famous quote' generator. Each time it's called you will get a different quote.
+// Built as a way to keep periodic messages, like emails and slack notifications from cron jobs interesting.
+//
+// Quotes are sourced from this list: https://github.com/umbrae/reddit-top-2.5-million/blob/master/data/quotes.csv
+//
+// API Built by Edd Turtle (designedbyaturtle.co.uk)
+//
+
 package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
-	"io"
 	"math/rand"
 	"net/http"
 	"os"
@@ -14,51 +24,89 @@ const (
 	CSV_FILE = "quotes.csv"
 )
 
-type CsvLine struct {
-	CreatedAt string
-	Score     string
-	Domain    string
-	Id        string
-	Title     string
+type ResponseContent struct {
+	Text string `json:"text",xml:"text"`
 }
 
 func main() {
+	// Setup
+	fmt.Println("Starting Server...")
+	rand.Seed(time.Now().UnixNano())
 	http.HandleFunc("/", indexHandler)
+	// Run
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
-	quote := getQuote()
-	fmt.Println(quote)
-	io.WriteString(w, quote[4])
+
+	returnType := getReponseContentType(req)
+	w.Header().Set("X-Powered-By", "Biscuits")
+	w.Header().Set("Content-Type", returnType+"; charset=UTF-8")
+
+	quote, err := getQuote()
+	if err != nil {
+		// TODO Show 500
+	}
+
+	var result []byte
+	if returnType == "application/json" {
+		// JSON
+		result, _ = json.Marshal(ResponseContent{Text: quote[4]})
+	} else if returnType == "text/xml" {
+		// XML
+		result, err = xml.Marshal(ResponseContent{Text: quote[4]})
+	} else {
+		// PLAIN
+		result = []byte(quote[4])
+	}
+
+	if err != nil {
+		// TODO Show 500
+	}
+
+	fmt.Fprintf(w, "%s", result)
 }
 
-func getQuote() []string {
-	quotes := getAllQuotes()
+func getReponseContentType(req *http.Request) (returnType string) {
+	returnType = "text/plain"
+	keys, ok := req.URL.Query()["accept"]
+	if ok && keys[0] == "json" {
+		returnType = "application/json"
+	}
+	if ok && keys[0] == "xml" {
+		returnType = "text/xml"
+	}
+	return
+}
+
+func getQuote() ([]string, error) {
+	quotes, err := getAllQuotes()
+	if err != nil {
+		return nil, err
+	}
 	randomNum := getRandomNum(len(quotes))
-	return quotes[randomNum]
+	return quotes[randomNum], nil
 }
 
 func getRandomNum(max int) int {
-	rand.Seed(time.Now().UnixNano())
 	randomNum := rand.Intn(max)
 	return randomNum
 }
 
-func getAllQuotes() [][]string {
+func getAllQuotes() ([][]string, error) {
 
 	// Open CSV file
 	f, err := os.Open(CSV_FILE)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer f.Close()
 
 	// Read File into a Variable
 	lines, err := csv.NewReader(f).ReadAll()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	return lines
+	return lines, nil
 }
